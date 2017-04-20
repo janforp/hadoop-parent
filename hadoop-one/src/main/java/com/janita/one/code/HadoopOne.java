488ca98350d1556f8280abf4d116a36583363a8b
Janita <zhuchenjian@huizhi-tech.com>
2017-04-20 13:35:22 +0800

    hadoop

---
+++ hadoop-one/src/main/java/com/janita/one/code/HadoopOne.java
@@ -0,0 +1,12 @@
+package com.janita.one.code;
+
+/**
+ * Created by Janita on 2017-04-20 11:30
+ */
+public class HadoopOne {
+
+    public static void main(String[] args){
+
+        System.out.println("*******");
+    }
+}
