a58930c194c203e9d7bcbab9c1bd9256458d4a8e
Janita <zhuchenjian@huizhi-tech.com>
2017-04-20 13:36:31 +0800

    hadoop

---
+++ hadoop-one/src/test/java/com/janita/one/HadoopOneTest.java
@@ -0,0 +1,7 @@
+package com.janita.one;
+
+/**
+ * Created by Janita on 2017-04-20 13:36
+ */
+public class HadoopOneTest {
+}
---
+++ hadoop-two/src/test/java/com/janita/two/HadoopTwoTest.java
@@ -0,0 +1,7 @@
+package com.janita.two;
+
+/**
+ * Created by Janita on 2017-04-20 13:36
+ */
+public class HadoopTwoTest {
+}
